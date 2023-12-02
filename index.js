7b9a6c89e443da2e0b938aa380895d18eee9b21f
nagashima-w <zhangdaoshe5@gmail.com>
2023-12-03 00:50:07 +0900

    テンプレートエンジン使用

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand', {useNewUrlParser: true, 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/dog", (req, res) => {
-  res.send("わんわん");
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  res.render("products/index", { products });
 });
 
 app.listen(3000, () => {
